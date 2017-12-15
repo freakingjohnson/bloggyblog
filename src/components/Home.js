6f5b59b7dc0f8d47ec2c42f234a722d06c55c79c
freakingjohnson <enjayification@gmail.com>
2017-12-15 15:46:13 -0700

    landing page changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import '../css/App.css';
-import { Card, CardHeader, CardTitle, CardText } from 'material-ui/Card';
+import { Card, CardHeader, CardTitle, CardText, CardMedia } from 'material-ui/Card';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import SwipeableViews from 'react-swipeable-views';
 import Avatar from 'material-ui/Avatar'
 import author from '../assets/author.jpg'
 import devPhoto from '../assets/devPhoto.jpg'
+import brasilPhoto from '../assets/picture.jpg'
 
 const styles = {
   headline: {
@@ -36,6 +37,20 @@ class Home extends Component {
     return (
       <div className="container">
         <h1 className="appName">Bem-vindo</h1>
+        <Card>
+          <CardTitle title="Card title" subtitle="Card subtitle" />
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+          <CardMedia
+            overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+          >
+            <img src={brasilPhoto} alt="" className="landingPic" />
+          </CardMedia>
+        </Card>
       </div>
 
     );
